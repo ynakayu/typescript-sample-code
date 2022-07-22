caa136a41fb577b2ac0c0ffb6328ec8d9a762ca2
naka <9111754+ynakayu@users.noreply.github.com>
2022-07-22 16:30:53 +0900

    03-10-compound-assignment-operator

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 // 足し算
-let num = 20;
+var num = 20;
 num = num + 10;
 // console.log(num);
-
-let num1 = 20;
+var num1 = 20;
 num1 += 10;
 // console.log(num1);
-
 // 引き算
-let num2 = 20;
+var num2 = 20;
 num2 -= 10;
 console.log(num2);
-
 // 掛け算
-let num3 = 20;
+var num3 = 20;
 num3 *= 10;
 console.log(num3);
-
 // 割り算
-let num4 = 20;
+var num4 = 20;
 num4 /= 10;
 console.log(num4);
-
-
-
