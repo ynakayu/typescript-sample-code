99cb343df356d8db9f5fd4a54498df06027eb22a
naka <9111754+ynakayu@users.noreply.github.com>
2022-07-22 11:20:35 +0900

    02-07-comment

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // console.log('Hello World!');
-
 /*
 console.log('Hello World!');
 console.log('Hello World!');
 console.log('Hello World!');
-*/
-
-// console.log('Hello World!');
-// console.log('Hello World!');
-// console.log('Hello World!');
+*/ 
