35c01d0794aad76835a3fbbb640a35e7491a8fbb
naka <9111754+ynakayu@users.noreply.github.com>
2022-07-22 12:12:23 +0900

    03-03-escape-sequence

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 console.log('Hello! I\'m Yuta Nakamura.');
-
 console.log('\¥ \t AAA \n BBB');
-
 console.log("Hello! I'm Yuta Nakamura.");
-
 console.log('こんにちは！"中村"さん');
-
-console.log('Hello');
-console.log("Hello");
