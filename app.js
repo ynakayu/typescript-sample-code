5f17e38ab8758a267e97c6c939f8b4ae69cd3e1e
naka <9111754+ynakayu@users.noreply.github.com>
2022-07-25 11:51:04 +0900

    03-14-exercise-operators

--- app.js
+++ app.js
@@ -3,19 +3,14 @@
 // ・体重が60kg
 // ・身長が1.7m
 // です。
-
 // BMIを計算して、consoleへ出力。
-
 // 補足：
 // BMI = 体重(kg) / (身長(m)  * 身長(m) )
-
 // BMI・・・肥満度を表す体格指数。
-
 // 出力は、このようなフォーマットで出力。
 // 例）
 // BMI: 20.761245674740486
-
-const mass = 60;
-const height = 1.7;
-const bmi = mass / (height * height);
-console.log(`BMI: ${bmi}`);
+var mass = 60;
+var height = 1.7;
+var bmi = mass / (height * height);
+console.log("BMI: ".concat(bmi));
