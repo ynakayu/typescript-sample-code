87fb35eb9e14009859a9e64e144345f53c773ea6
naka <9111754+ynakayu@users.noreply.github.com>
2022-07-25 11:28:52 +0900

    03-13-increment-decrement

--- app.js
+++ app.js
@@ -2,21 +2,16 @@
 // let num = 10;
 // num++;
 // console.log(num);
-
 // num--;
 // console.log(num);
-
 // // 前置演算子
 // ++num;
 // console.log(num);
-
 // --num;
 // console.log(num);
-
-let num1 = 10;
-let num2 = 10;
-let ans1 = ++num1;
-let ans2 = num2++;
-
+var num1 = 10;
+var num2 = 10;
+var ans1 = ++num1;
+var ans2 = num2++;
 console.log(ans1);
 console.log(ans2);
