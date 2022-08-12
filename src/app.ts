a972ac19cc167c169a0bf08097e4a15a42a183ab
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-12 15:16:48 +0900

    05-04-various-comparison-operators

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+// 以上
+const a = 100;
+if(a >= 100) {
+  console.log('100以上');
+}
+
+// より大きい
+const b = 101;
+if(b > 100) {
+  console.log('100より大きい');
+}
+
+// 以下
+const c = 100;
+if(c <= 100) {
+  console.log('100以下');
+}
+
+// 未満
+const d = 99;
+if(d < 100) {
+  console.log('100未満');
+}
+
+// 等しい
+const e = 100;
+if(e === 100) {
+  console.log('100と等しい');
+}
+
+// 等しくない
+let f = 111;
+if(f !== 100) {
+  console.log('等しくない');
+}
+
