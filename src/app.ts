c47dfe58b5299baa1ba944f53ba9c2648f927f01
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-12 13:06:33 +0900

    05-02-if-else

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('Hello app.ts!!!!! 123');
+const temperature = 28;
+
+if(temperature > 28) { // 比較演算子、true / false
+  console.log('ON!');
+} else {
+  console.log('OFF!');
+}
+
+console.log(29 > 28); // true
+console.log(28 > 28); // false
