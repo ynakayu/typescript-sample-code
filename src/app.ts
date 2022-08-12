63cb7be1f9d5de51f68139482b2b89b8442cdaf8
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-12 16:04:43 +0900

    05-06-logical-operator

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// && 論理AND
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// || 論理OR
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// ! 論理NOT
+// console.log(!true);
+// console.log(!false);
+
+// ! && ||
+
+console.log(false && true || true);
+console.log(false && (true || true));
