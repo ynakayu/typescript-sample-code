a2fe4eaf132c70e8b9c0515e3f62570859babb11
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-15 16:54:27 +0900

    06-08-default-value-of-argument

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+const getRectangle = (height: number, width: number = 10) => {
+  return height * width;
+};
+
+console.log(getRectangle(3, 5));
+
+// const getRectangle = (height: number = 10, width: number) => {
+//   return height * width;
+// };
+
+// console.log(getRectangle(,10));
