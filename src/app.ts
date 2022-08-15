83f4f999c29846253c66508557604d586d3bf7d1
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-15 13:34:28 +0900

    06-03-function-declarations

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+function getRectangle(height: number, width: number) {
+  return height * width;
+}
+
+console.log(getRectangle(3, 5));
+// console.log(rectangle('abc', 5));
+
+function hello() {
+  console.log('Hello!');
+}
+
+hello();
