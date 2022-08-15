27c31f70c63521571be4149507efa7acead296fc
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-15 16:01:36 +0900

    06-05-arrow-function

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+// const getRectangle = function(height: number, width: number) {
+//   return height * width;
+// };
+
+// console.log(getRectangle(3, 5));
+
+const getRectangle = (height: number, width: number) => {
+  return height * width;
+};
+
+console.log(getRectangle(3, 5));
+
+// const getRectangle = (height: number, width: number) => height * width;
+// console.log(getRectangle(3, 5));
+
+// const putHeight = (height: number) => console.log(`${height}`);
+// putHeight(3);
+
+const hello = () => console.log('Hello!');
+hello();
