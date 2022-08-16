58953127d32df0b5639b9480bf63591dd67ea45a
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-16 16:27:07 +0900

    08-02-for

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+// 例題1)繰り返し処理forを使って、コンソールに1から3まで順に表示する
+// for(let i = 1; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// 例題2) 配列['a', 'b', 'c']の値を、
+// for文を利用して、1つずつコンソールに出力
+
+const colors = ['red', 'blue', 'green'];
+// for(let i = 0; i < 3; i++) {
+//   console.log(colors[i]);
+// }
+
+for(let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
