45af9b6fa0496202920ab35cce09aa922369f616
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-16 09:46:49 +0900

    07-02-array-basics

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+// const colors: string[] = ['Red', 'Green', 'Blue'];
+// const colors: string[] = [1, 2, 3]; // NG
+const colors = ['Red', 'Green', 'Blue'];
+console.log(colors);
+
+// const mixedArr = ['abc', 1, true, false];
+// console.log(mixedArr);
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+// console.log(colors[3]); // undefined
+
+console.log(colors.length);
+console.log(colors[colors.length - 1]);
+
+const emptyArr: number[] = [];
+console.log(emptyArr);
