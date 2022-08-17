412d844a256e404bd292e35c8705173cd5bfe90c
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-17 11:12:32 +0900

    clear

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-const colors = ['Red', 'Green', 'Blue'];
-for(const color of colors) {
-  console.log(color);
-}
-
-const str = 'TypeScript';
-for(const character of str) {
-  console.log(character);
-}
