e6d16582599f3925bc220429333f409971bba52a
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-31 10:59:35 +0900

    09-06-constructor

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 class User {
-  name: string = '';
+  readonly name: string = '';
   age: number = 0;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  isAdult(): boolean {
+    return this.age >= 18;
+  }
+
+  // setName(name: string) {
+  //   this.name = name;
+  // }
 }
 
-const olivia = new User();
+const olivia = new User('Olivia', 23);
 console.log(olivia.name);
 console.log(olivia.age);
+console.log(olivia.isAdult());
 
-olivia.name = 'Olivia';
-olivia.age = 23;
-console.log(olivia.name);
-console.log(olivia.age);
