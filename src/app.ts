61c07ee3e6df4d15a43ed9cf7cf612f1db38146b
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-31 10:25:27 +0900

    09-04-readonly-property

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 class User {
-  name: string = '';
+  readonly name: string = 'Yuta Nakamura';
   age: number = 0;
 }
 
-const olivia = new User();
-console.log(olivia.name);
-console.log(olivia.age);
-
-olivia.name = 'Olivia';
-olivia.age = 23;
-console.log(olivia.name);
-console.log(olivia.age);
+const nakamura = new User();
+// nakamura.name = 'Olivia'; // error
