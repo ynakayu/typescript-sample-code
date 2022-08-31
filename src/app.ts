fe5848810f699bd6d7810514cc1b1ac89f2380bd
naka <9111754+ynakayu@users.noreply.github.com>
2022-08-31 10:45:48 +0900

    09-05-メソッドを使おう

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 class User {
   name: string = '';
   age: number = 0;
+
+  isAdult(): boolean {
+    return this.age >= 18;
+  }
+
+  setAge(age: number) {
+    this.age = age;
+  }
 }
 
 const olivia = new User();
-console.log(olivia.name);
 console.log(olivia.age);
+console.log(olivia.isAdult());
 
-olivia.name = 'Olivia';
-olivia.age = 23;
-console.log(olivia.name);
+olivia.setAge(23);
 console.log(olivia.age);
+console.log(olivia.isAdult());
+
+const liam = new User();
+liam.setAge(18);
+console.log(liam.age);
+console.log(liam.isAdult());
